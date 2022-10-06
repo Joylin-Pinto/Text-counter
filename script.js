93658139563ee36baac8f224491f915a5cea1072
Joylin Pinto <73455388+Joylin-Pinto@users.noreply.github.com>
2022-10-06 17:36:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let stats = document.querySelector('.status');
+let text = document.getElementById('text');
+let char = document.getElementById('char');
+let words = document.getElementById('words');
+let lines = document.getElementById('lines');
+let symbols = document.getElementById('symbols');
+
+function count() {
+    if(text.value.length === 0){
+        stats.style.display = "none";
+    }
+    else{
+        stats.style.display = "block";
+        char.innerHTML = text.value.length + " Characters   ";
+        words.innerHTML = text.value.trim().split(/\s+/).length + " words   ";
+        lines.innerHTML = text.value.split("\n").length + " lines   ";
+        symbols.innerHTML = text.value.split(/[[!@#$%^&*+()_={};:'"<>.,?/-]/).length + " symbols    ";
+    }
+}
+text.addEventListener('input', count);
